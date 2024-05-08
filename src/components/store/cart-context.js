0b778e93ab38cd62e08f832f7ccd59f77a8c5911
jeongwon <tjtkdvl@gmail.com>
2024-05-08 16:47:21 +0900

    Cart 담기 기능 및 Modal 렌더링

--- src/components/store/cart-context.js
+++ src/components/store/cart-context.js
@@ -1,10 +1,9 @@
 import React from 'react';
 // 장바구니에 담기거나 제외되는 항목들을 전역 상태 관리하는 컨텍스트
 // 컨텍스트에 들어가는 초기 객체는 뭘 담을 것인지에 대한 정의
-
 const CartContext = React.createContext({
-  items: [],
-  addItem: (item) => {},
+  items: [], // 장바구니에 담긴 항목 배열
+  addItem: (item) => {}, // 주문 총액
   removeItem: (id) => {},
 });
 
