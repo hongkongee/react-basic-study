b8fc50ed7db3483cdd14172f8da31e7234663c07
jeongwon <tjtkdvl@gmail.com>
2024-05-08 11:25:24 +0900

    Food 템플릿 작성중 (Header button 부분)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MainHeader from './components/SideEffect/MainHeader/MainHeader';
 import Login from './components/SideEffect/Login/Login';
 import Home from './components/SideEffect/Home/Home';
 import AuthContext from './components/store/auth-context';
+import Header from './components/Food/Layout/Header';
 
 function App() {
   console.log('App 컴포넌트 실행!');
@@ -44,17 +45,9 @@ function App() {
   console.log('App 컴포넌트 끝!');
 
   return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn,
-      }}
-    >
-      <MainHeader onLogout={logoutHandler} />
-      <main>
-        {isLoggedIn && <Home />}
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-      </main>
-    </AuthContext.Provider>
+    <>
+      <Header />
+    </>
   );
 }
 
--- src/components/Food/Layout/Header.js
+++ src/components/Food/Layout/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import styles from './Header.module.scss';
+import mealsImage from '../../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
+  const { header, 'main-image': mainImage } = styles;
   return (
     <>
-      <header className={styles.header}>
+      <header className={header}>
         <h1>ReactMeals</h1>
+        <HeaderCartButton />
         <button>Cart</button>
       </header>
-      <div className={styles['main-image']}>
-        <img src="../../../assets/meals.jpg" alt="Looks very delicious meals" />
+      <div className={mainImage}>
+        <img src={mealsImage} alt="Looks very delicious meals" />
       </div>
     </>
   );
 };
-export default header;
+export default Header;
